c56d7bf16edaf60da436835c81be120b9d9853c1
Amir <amirsanni@gmail.com>
2019-04-02 21:36:02 +0100

    Update send_email.php

--- tests/send_email.php
+++ tests/send_email.php
@@ -12,6 +12,7 @@ $mail->recipient = ['amirsanni@gmail.com'];
 $mail->recipient_name = "Amir Sanni";
 $mail->cc = ['john@doe.com', 'doe@john.com'];//single string is also acceptable
 $mail->bcc = 'foo@bar.com';//an array of email addresses is also acceptable
-$mail->attach = ['file1', 'file2', 'file3'];//single string is also acceptable
+$mail->attach = ['file1', 'file2', 'file3'];//single string is also acceptable  
+$mail->send_as_email = 'john@doe.com';
 
 $mail->send();
