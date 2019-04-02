21dfda1a9e501292301c4c3051318a9ef470ff0a
Amir Sanni <amirsanni@gmail.com>
2019-04-02 16:47:04 +0100

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'jamesiarmes\\PhpNtlm\\' => array($vendorDir . '/jamesiarmes/php-ntlm/src'),
     'jamesiarmes\\PhpEws\\' => array($vendorDir . '/php-ews/php-ews/src'),
+    'amirsanni\\phpewswrapper\\' => array($baseDir . '/src'),
 );
