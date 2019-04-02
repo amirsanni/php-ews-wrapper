bfd032f6b1e9938b53683fb4f6c738610546d299
Amir Sanni <amirsanni@gmail.com>
2019-04-02 17:25:40 +0100

    Update send_email.php

--- tests/send_email.php
+++ tests/send_email.php
@@ -14,4 +14,4 @@ $mail->cc = ['john@doe.com', 'doe@john.com'];//single string is also acceptable
 $mail->bcc = 'foo@bar.com';//an array of email addresses is also acceptable
 $mail->files = ['file1', 'file2', 'file3'];//single string is also acceptable
 
-echo $mail->send();
+$mail->send();
