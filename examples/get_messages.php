66b81b1b761f3299968af1d7980f34eab28892e7
Amir Sanni <amirsanni@gmail.com>
2019-04-03 17:48:33 +0100

    Create get_messages.php

---
+++ examples/get_messages.php
@@ -0,0 +1,20 @@
+<?php
+
+require "vendor/autoload.php";
+
+use amirsanni\phpewswrapper\PhpEwsWrapper;
+
+$mail = new PhpEwsWrapper('amir.sanni@mainone.net', 'Razafindrakoto10');
+
+$mail->per_page = 30;
+
+$mail->getInboxMessages();//Messages in inbox
+$mail->getSentItems();
+$mail->getDraftItems(1);
+$mail->getOutboxItems(1);
+$mail->getFavourites(1);
+$mail->getJunkItems();
+$mail->getDeletedMessages();
+$mail->getArchivedMessages();
+$mail->getContacts();
+$mail->getTasks();
