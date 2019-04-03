33958b857e88533cc3240f95de9d385970c8bcaa
Amir Sanni <amirsanni@gmail.com>
2019-04-03 09:33:27 +0100

    Create create_draft.php

---
+++ tests/create_draft.php
@@ -0,0 +1,18 @@
+<?php
+require "../vendor/autoload.php";
+
+use amirsanni\phpewswrapper\PhpEwsWrapper;
+
+$mail = new PhpEwsWrapper('email', 'password', 'optionalServerAddress');
+
+$mail->sender_name = "Amir Sanni";
+$mail->subject = "Package Test";
+$mail->message = "Test email";
+$mail->recipient = 'amirsanni@gmail.com';//an array of email addresses is also acceptable
+$mail->recipient_name = "Amir Sanni";
+$mail->cc = ['john@doe.com', 'doe@john.com'];//single string is also acceptable
+$mail->bcc = 'foo@bar.com';//an array of email addresses is also acceptable
+$mail->attach = ['file1', 'file2', 'file3'];//single string is also acceptable  
+$mail->send_as_email = 'john@doe.com';
+
+$mail->createDraft();
